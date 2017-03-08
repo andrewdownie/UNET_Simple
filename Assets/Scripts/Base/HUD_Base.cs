a6382a0b7851c3647f28f60b6049ec1ff9fbefe3
andrew downie <andrew_dev@outlook.com>
2017-03-07 23:34:20 -0500

    back to basics

--- Assets/Scripts/Base/HUD_Base.cs
+++ Assets/Scripts/Base/HUD_Base.cs
@@ -28,9 +28,9 @@ public abstract class HUD_Base<T> : MonoBehaviour where T : HUD_Base<T>  {
     /////
     void Awake()
     {
-        if (FindObjectsOfType(typeof(Canvas)).Length > 1)
+        if (FindObjectsOfType(typeof(HUD)).Length > 1)
         {
-            Debug.LogWarning("Destroying excess Canvas's...");
+            Debug.LogWarning("Destroying excess HUD's...");
             Destroy(gameObject);
         }
 
--- Assets/Scripts/Base/MuzzleFlash_Base.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class MuzzleFlash_Base : MonoBehaviour {
-	public abstract void ShowFlash();
-
-	public abstract void ShowFlash(float duration);
-}
